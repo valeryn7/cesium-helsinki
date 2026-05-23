9851647a3e244324f17b819d9533ffaab22e174a
valerynaterchocho@gmail.com <valerynaterchocho@gmail.com>
2026-05-23 11:14:00 -0300

    Add citydb tools and pg2b3dm binary

---
+++ citydb-tool-1.3.1/3dcitydb/postgresql/shell-scripts/unix/connection-details.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Provide your database details here ------------------------------------------
+export PGBIN=/usr/local/bin/psql
+export PGHOST=localhost
+export PGPORT=5432
+export CITYDB=citydb_test
+export PGUSER=valerynater
+#------------------------------------------------------------------------------
